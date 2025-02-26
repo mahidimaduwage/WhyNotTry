e8f0cad977223ad1fe302ddcd6e01d9d778308c4
mahidimaduwage <mahidimaduwage@gmail.com>
2025-02-26 10:47:05 +0530

    Initial Commit

---
+++ WhyNotTry/WhyNotTryApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhyNotTryApp.swift
+//  WhyNotTry
+//
+//  Created by Mahidi Maduwage on 2025-02-26.
+//
+
+import SwiftUI
+
+@main
+struct WhyNotTryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
