680a642e0a297d94279c221054dc9263c23996f7
mahidimaduwage <mahidimaduwage@gmail.com>
2025-02-26 12:14:51 +0530

    basic ui changes

--- WhyNotTry/ContentView.swift
+++ WhyNotTry/ContentView.swift
@@ -8,12 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    var data = ["Archery", "Baseball", "Basketball", "Bowling", "Rugby"]
+    var colors: [Color] = [.blue, .red, .green, .pink, .orange]
+    
+    @State var selected = "Archery"
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Why Not Try !!!!")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+            
+            Circle()
+                .fill(colors.randomElement() ?? .black)
+                .overlay {
+                    Image(systemName: "figure.\(selected.lowercased())")
+                        .font(.system(size: 144))
+                        .foregroundColor(.white)
+                }
+            
+            Text("\(selected)")
+                .font(.title)
+            
+            Button("Try Again") {
+                selected = data.randomElement() ?? "Not Found"
+            }
+            .buttonStyle(.borderedProminent)
         }
         .padding()
     }
